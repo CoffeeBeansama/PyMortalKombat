0253d0e9ad6a042b0342dee8892e0d086db914d1
Francis Aigo <103745801+CoffeeBeansama@users.noreply.github.com>
2023-10-08 21:46:04 +0800

    update

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(pg.sprite.Sprite):
         super().__init__(group)
 
         self.pos = pos
-        self.image = pg.Surface((10, 10))
+        self.image = loadSprite("Sprites/Player/Idle/00.png",(350,250)).convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0,0)
 
@@ -19,9 +19,11 @@ class Player(pg.sprite.Sprite):
     def getInputs(self):
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
-            print("left")
+            self.image = pg.transform.flip(self.image, True, False).convert_alpha()
+
+
         elif keys[pg.K_d]:
-            print("right")
+            self.image = pg.transform.flip(self.image, False, False).convert_alpha()
 
     def update(self):
         self.getInputs()
