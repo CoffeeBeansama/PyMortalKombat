59eb207fed3a31896648d108e61f22f9dcdf9968
Francis Aigo <103745801+CoffeeBeansama@users.noreply.github.com>
2023-10-11 09:33:13 +0800

    update

--- server.py
+++ server.py
@@ -15,29 +15,30 @@ s.listen(2)
 print("Waiting for connection...Server started!")
 
 def threadedClient(conn):
+    conn.send(str.encode("Hello from server"))
     reply = ""
     while True:
         try:
             data = conn.recv(2048)
             reply = data.decode("utf-8")
-
             if not data:
                 print("Connection Disconnected!!")
                 break
             else:
-                print(f"Received: {reply}")
-                print(f"Sending: {reply}")
-
+                print(f"Received: {reply} from client")
+                print(f"Sending: {reply} to client")
             conn.sendall(str.encode(reply))
-
         except:
             break
 
+    print("Connection lost")
+    conn.close()
 
 while True:
     conn,addr = s.accept()
+    print("this")
     print(f"Connected to: {addr}")
 
-    start_new_thread(threadedClient,(conn,addr))
+    start_new_thread(threadedClient,(conn,))
 
 
